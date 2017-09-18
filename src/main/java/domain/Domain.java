7b99f3ae300316457a4f2b9c1c6b6b12c1f91ee3
Jesus <deleterr1996@ukr.net>
2017-09-19 01:57:40 +0300

    Editing Domain class for testing all code.

--- src/main/java/domain/Domain.java
+++ src/main/java/domain/Domain.java
@@ -5,8 +5,12 @@ import entity.Address;
 import entity.Employee;
 import entity.Project;
 import org.hibernate.Session;
+import service.AddressService;
+import service.EmployeeService;
+import service.ProjectService;
 
 import java.sql.Date;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,16 +19,16 @@ import java.util.Set;
  * Created by Jesus on 17.09.2017.
  */
 public class Domain {
-    public static void main(String [] args){
-        Session session = HibernateUtil.getSessionFactory().openSession();
-
-        session.beginTransaction();
+    public static void main(String [] args) throws SQLException {
+        AddressService addressService = new AddressService();
+        EmployeeService employeeService = new EmployeeService();
+        ProjectService projectService = new ProjectService();
 
         Address address = new Address();
         address.setCountry("Ukraine");
         address.setCity("Kyiv");
         address.setStreet("Pidvysotskogo");
-        address.setPostCode("10014");
+        address.setPostCode("01014");
 
         Project project = new Project();
         project.setTitle("Kyiv UA");
@@ -47,11 +51,10 @@ public class Domain {
         projects.add(project);
         employee.setProjects(projects);
 
-        session.save(address);
-        session.save(project);
-        session.save(employee);
+        addressService.add(address);
+        employeeService.add(employee);
+        projectService.add(project);
 
-        session.getTransaction().commit();
         HibernateUtil.shutdown();
     }
 }
