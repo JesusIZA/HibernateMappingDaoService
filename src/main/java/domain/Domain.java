197d0c43628a6c943a848dd8f3324563950cebc5
Jesus <deleterr1996@ukr.net>
2017-09-19 01:59:05 +0300

    Editing Domain class for testing all code.

--- src/main/java/domain/Domain.java
+++ src/main/java/domain/Domain.java
@@ -4,7 +4,6 @@ import bl.HibernateUtil;
 import entity.Address;
 import entity.Employee;
 import entity.Project;
-import org.hibernate.Session;
 import service.AddressService;
 import service.EmployeeService;
 import service.ProjectService;
